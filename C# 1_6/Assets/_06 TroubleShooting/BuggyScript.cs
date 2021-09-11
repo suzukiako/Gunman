01c4025cdbe21774c88d036c7aae66edeed827cf
パンくん\vantan <mkzkgsabbm@i.softbank.jp>
2021-09-11 18:52:08 +0900

    Nullチェック

---
+++ C# 1_6/Assets/_06 TroubleShooting/BuggyScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class BuggyScript : MonoBehaviour
+{
+    /// <summary>左クリックで切り替えるスプライト</summary>
+    [SerializeField] Sprite[] m_sprites = default;
+    /// <summary>右クリックで消す（非アクティブにする）GameObject の名前</summary>
+    [SerializeField] string m_targetObjectName = default;
+    SpriteRenderer m_sprite = default;
+    int m_index;
+
+    void Start()
+    {
+        // GetComponent<T>() は自分と同じ GameObject に
+        // 追加されているコンポーネント T を返すが、
+        // 指定されたコンポーネントが追加されていない時は null を返す。
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 左クリックでスプライトを切り替える
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_sprite.sprite = m_sprites[m_index % 3];
+            m_index++;
+            Debug.Log("Index: " + m_index % 3);
+        }
+
+        // 右クリックで指定した名前のオブジェクトを非アクティブにする（消す）
+        if (Input.GetButtonDown("Fire2") && m_targetObjectName.Length > 0)
+        {
+            // GameObject.Find() は指定した名前の GameObject を検索して返すが、
+            // その名前の GameObject が見つからない時は null を返す。
+            m_targetObjectName = "Gunman";
+            GameObject go = GameObject.Find(m_targetObjectName);
+            go.SetActive(false);
+           
+        }
+    }
+}
---
+++ C# 1_6/Assets/_06 TroubleShooting/PunchController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// パンチを制御するコンポーネント
+/// Fire1 でダッシュする
+/// ぶつかったものを弾き飛ばす
+/// </summary>
+public class PunchController : MonoBehaviour
+{
+    /// <summary>移動の時にかける力</summary>
+    [SerializeField] float m_movePower = 5f;
+    /// <summary>ダッシュする力</summary>
+    [SerializeField] float m_dashPower = 15f;
+    /// <summary>触れたものを吹き飛ばす力</summary>
+    [SerializeField] float m_punchPower = 15f;
+    Rigidbody2D m_rb = default;
+    float m_h, m_v;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 方向の入力を受け取る
+        m_h = Input.GetAxisRaw("Horizontal");
+        m_v = Input.GetAxisRaw("Vertical");
+
+        // Fire1 でダッシュする
+        if (Input.GetButtonDown("Fire1"))
+        {
+            m_rb.AddForce(this.transform.up * m_dashPower, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // 入力に応じて移動し、向きを変える
+        Vector2 dir = new Vector2(m_h, m_v).normalized;
+        m_rb.AddForce(dir * m_movePower);
+        this.transform.up = m_rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();  // 衝突相手の Rigidbody2D コンポーネントを取得する
+        // パンチが当たった時に吹っ飛ばす
+        if (rb != null)
+        {
+            Vector2 dir = (collision.gameObject.transform.position - this.transform.position).normalized;
+            rb.AddForce(dir * m_punchPower, ForceMode2D.Impulse);
+        }
+        
+        
+    }
+}
